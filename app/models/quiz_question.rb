369792ec979f08eb748b81a3db6650fc0c6fffd8
Joao Mangilli <joao-luis-mangilli@hotmail.com>
2024-05-08 16:03:22 -0300

    Fix division by zero

--- app/models/quiz_question.rb
+++ app/models/quiz_question.rb
@@ -11,16 +11,17 @@ class QuizQuestion < ApplicationRecord
 
   private
 
-  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
   def weight_must_be_integer
     return unless question.present? && weight.present?
 
     correct_alternatives = question.alternatives.select(&:correct?).count
     incorrect_alternatives = question.alternatives.reject(&:correct?).count
 
-    return unless (weight % correct_alternatives).positive? || (weight % incorrect_alternatives).positive?
+    return unless (correct_alternatives.positive? && (weight % correct_alternatives).positive?) ||
+                  (incorrect_alternatives.positive? && (weight % incorrect_alternatives).positive?)
 
     errors.add(:base, :weight_must_be_integer)
   end
-  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 end
